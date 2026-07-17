db082f37be25e021196c33d2b4b23d3140acd919
Amado-Jesus <84112314+Amado-Jesus@users.noreply.github.com>
2026-07-17 15:51:58 -0600

    Refactor app.py for clarity and structure

--- app.py
+++ app.py
@@ -146,11 +146,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-
-    flask_app.run(
-        debug=False,
-        host="0.0.0.0",
-        port=port
-    )
+    flask_app.run(debug=True, host="0.0.0.0", port=8080)
+    
     
